f67e88f4362c0afd618651c8edb530452724aa60
Patil Rushab Ramhari <125505609+RUSHAB3979@users.noreply.github.com>
2026-08-25 23:01:22 +0530

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+private:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!p || !q || p->val != q->val) return false;
+        
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!subRoot) return true;
+        if (!root) return false;
+        
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
